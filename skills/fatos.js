7dc306919d295e01e64dd5078fb66e82445d8e31
Dev <desenvolvimento01@rocketon.app>
2019-04-04 16:51:47 -0300

    Listar todos os fatos

--- skills/fatos.js
+++ skills/fatos.js
@@ -1,7 +1,24 @@
+const database = require('../data/database')
+
 module.exports = function(controller) {
 
+    // Lista todos os fatos
     controller.hears(['fatos', 'variaveis'], 'message_received', function(bot, message) {
-        bot.reply(message, 'Devo listar todos os fatos aqui.');
-    });
+        const fatos = database.fatos
+        let resposta = ''
 
+        if (fatos < 1) {
+            return bot.reply(message, 'Ainda não temos fatos cadastrados no nosso sistema especialista. :(')
+        }
+        
+        resposta = 'Estes são todos os fatos cadastrados no nosso sistema especialista: \n\n'
+
+        for(fato of fatos) {
+            resposta = resposta + `* ${fato.Nome} \n`
+        }
+
+        resposta = resposta + '\n Para detalhar um fato basta escrever `fato NomeDoFato`'
+
+        bot.reply(message, resposta)
+    });
 }
