f6bebe5114a1f3d1109f1705a1544434280cd17c
Dev <desenvolvimento01@rocketon.app>
2019-04-19 18:21:30 -0300

    Fazendo as perguntas da regra

--- skills/regras.js
+++ skills/regras.js
@@ -1,56 +1,39 @@
+const database = require('../data/database'),
+    Fuse = require('fuse.js');
+
 module.exports = function(controller) {
-    // Chaining multiple questions together using callbacks
-    // You have to call convo.next() in each callback in order to keep the conversation flowing
-    controller.hears('qq', 'message_received', function(bot, message) {
-        bot.startConversation(message, function(err, convo) {
-            convo.say('Lets get to know each other a little bit!')
+    
+    // Play em uma regra
+    controller.hears(['play (.*)', 'start (.*)', 'começar (.*)'], 'message_received', function(bot, message) {
+        const regras = database.regras
+        regra = message.match[1],
 
-            convo.ask('Which is more offensive? Book burning or flag burning?', function(res, convo) {
-                convo.next()
+             options = {
+                keys: ['Nome'],
+                threshold: 0.2,
+              },
+                fuse = new Fuse(regras, options),
+                pesquisado = fuse.search(regra)[0]
 
-                convo.ask('How often do you keep your promises?', function(res, convo) {
-                    convo.next()
-
-                    convo.ask('Which is bigger? The Sun or the Earth?', function(res, convo) {
-
-                        convo.say('Thank you, that is all for now')
-                        convo.next()
-
-                    })
-                })
-            })
-        })
-    })
-
-    // Method using threads and addQuestion
-    // Helps you avoid callback hell
-    // Docs for addQuestion https://github.com/howdyai/botkit/blob/master/readme.md#convoaddquestion
-    // Don't forget to pass an empty object after the callback and before the thread you're adding the question to!
-    controller.hears('regras123', 'message_received', function(bot, message) {
+                console.log(pesquisado)
+        
         const respostas = []
+        
         bot.createConversation(message, (err, convo) => {
             
-            convo.addMessage('Charmed to meet you, lets get to know one another!')
+            convo.addMessage(`Ei humano, nós vamos testar a regra **${pesquisado.Nome}**. Vamos começar!`)
             
-            convo.addQuestion('How much do you like robots?', (res, convo) => {
-                respostas.push(res)
-                convo.gotoThread('q2')
-            }, {}, 'default')
+            for (const pergunta in pesquisado.Condicao) {
+                const proxima = pesquisado.Condicao.length === parseInt(pergunta, 10) + 1 ? 'end' : `p${parseInt(pergunta, 10) + 1}`
+                const atual = pesquisado.Condicao[pergunta].Primeiro ? 'default' : `p${pergunta}`
+
+                convo.addQuestion(`${pesquisado.Condicao[pergunta].Fato.Nome} ?`, (res, convo) => {
+                    respostas.push(res)
+                    convo.gotoThread(proxima)
+                }, {}, atual)            
+            }           
             
-            
-            convo.addQuestion('Do you like your job?', (res, convo) => {
-                respostas.push(res)
-                convo.gotoThread('q3')
-            }, {}, 'q2')
-            
-            convo.addQuestion('How much glucose and energy does your body generate per hour?', (res, convo) => {
-                respostas.push(res)
-                tratarRespostas(respostas)
-                convo.gotoThread('end')
-            }, {}, 'q3')
-            
-            
-            convo.addMessage('Okay thank you very much for the valuable info, human.', 'end')
+            convo.addMessage('Okay, muito obrigado pelas informações, humano. Aguarde um instante', 'end')
             
             convo.activate();
         })
