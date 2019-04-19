a830e3422c27d5d98736fa2b252a08edfca69f3c
Dev <desenvolvimento01@rocketon.app>
2019-04-19 20:50:54 -0300

    Play na regra funcionando!

--- skills/regras.js
+++ skills/regras.js
@@ -21,7 +21,7 @@ module.exports = function(controller) {
         
         bot.createConversation(message, (err, convo) => {
             
-            convo.addMessage(`Ei humano, nós vamos testar a regra **${pesquisado.Nome}**. Vamos começar!`)
+            convo.addMessage(`Nós vamos testar a regra **${pesquisado.Nome}**. Vamos começar!`)
             
             for (const pergunta in pesquisado.Condicao) {
                 const proxima = pesquisado.Condicao.length === parseInt(pergunta, 10) + 1 ? 'end' : `p${parseInt(pergunta, 10) + 1}`
@@ -29,17 +29,34 @@ module.exports = function(controller) {
 
                 convo.addQuestion(`${pesquisado.Condicao[pergunta].Fato.Nome} ?`, (res, convo) => {
                     respostas.push(res)
+                    if (proxima === 'end') {
+                        tratarRespostas(respostas)
+                    }
                     convo.gotoThread(proxima)
                 }, {}, atual)            
             }           
             
-            convo.addMessage('Okay, muito obrigado pelas informações, humano. Aguarde um instante', 'end')
+            convo.addMessage('Tudo ok e ready for rock n roll!', 'end')
             
             convo.activate();
         })
                 
         const tratarRespostas = (respostas) => {
-            console.log("TCL: tratarRespostas -> respostas", respostas)
+            let resultadoRegra = true
+
+            for (const pergunta in pesquisado.Condicao) {
+                if (pesquisado.Condicao[pergunta].Resposta.Descricao.toLowerCase() !== respostas[pergunta].text) {
+                    resultadoRegra = false
+                }
+            }
+            
+            if (resultadoRegra) {
+                bot.reply(message, `Deu certo! Testamos a regra **${pesquisado.Nome}**, e o resultado obtido foi: \n\n 
+                ${pesquisado.condicaoObjetivos[0].Fato.Nome} é ${pesquisado.condicaoObjetivos[0].Resposta.Descricao}
+                `)
+            } else {
+                bot.reply(message, `Testamos a regra **${pesquisado.Nome}**, e com o valor das suas respostas não podemos confirmar que ${pesquisado.condicaoObjetivos[0].Fato.Nome} é ${pesquisado.condicaoObjetivos[0].Resposta.Descricao} :(`)
+            }
         }
     })
 
